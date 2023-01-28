48b4878545680ffa774f0554ff558b1696a90db5
ozgencmardi <ozgencmardi@gmail.com>
2023-01-28 18:46:23 +0000

    Index and Style.css updated. For loop created for script.js

--- script.js
+++ script.js
@@ -3,3 +3,39 @@ $(document).ready(function() {
     $("#currentDay").text(today);
 });
 
+
+for (let i = 9; i <= 17; i++) {
+
+    let row = $("<div>").addClass("time-block");
+    row.attr("id", i);
+
+    let hourCol = $("<div>").addClass("col-1 hour");
+    hourCol.text(i + ":00");
+
+    let eventCol = $("<div>").addClass("col-10 event");
+    let eventInput = $("<textarea>").addClass("description");
+    eventCol.append(eventInput);
+
+    let saveCol = $("<div>").addClass("col-1 save");
+    let saveBtn = $("<button>").addClass("saveBtn").text("Save");
+    saveCol.append(saveBtn);
+
+    row.append(hourCol, eventCol, saveCol);
+
+    $(".container").append(row);
+
+    let currentHour = moment().hour();
+
+    if (i < currentHour) {
+        
+        eventInput.addClass("past");
+
+    } else if (i === currentHour) {
+
+        eventInput.addClass("present");
+
+    } else {
+
+        eventInput.addClass("future");
+    }
+}
